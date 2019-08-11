6f88b2ec6c004465223a330c8c331a9bcea2f5a4
Seidi Nakamura <seidi.nakamura@st-ventures.jp>
2019-08-11 19:16:00 +0900

    AlertDialog (1)

---
+++ q15/app/src/main/java/kotlins/basic/q15/AlertDialogUtil.kt
@@ -0,0 +1,25 @@
+package kotlins.basic.q15
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+
+object AlertDialogUtil {
+    interface DialogUtilListener {
+        fun onPositive()
+        fun onNegative()
+    }
+
+    fun createOneButtonDialog(activity: AppCompatActivity, title: Int, messageId: Int, listener: DialogUtilListener?) {
+        val customDialog = CustomDialog(activity)
+        customDialog.dialogTitle = activity.getString(title)
+        customDialog.dialogMessage = activity.getString(messageId)
+        customDialog.isCancelButton = true
+        customDialog.onOkClickListener = DialogInterface.OnClickListener { _, _ ->
+            listener?.onPositive()
+        }
+        customDialog.onCancelClickListener = DialogInterface.OnClickListener { _, _ ->
+            listener?.onNegative()
+        }
+        customDialog.openDialog(activity.supportFragmentManager)
+    }
+}
